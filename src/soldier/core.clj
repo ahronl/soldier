007215b6d4346b7aeb4107527e10b975c1d9ef13
ahronl <aharon.levi@amobee.com>
2021-08-18 12:49:10 +0300

    implement game

--- src/soldier/core.clj
+++ src/soldier/core.clj
@@ -1,6 +1,76 @@
 (ns soldier.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def size 3)
+
+(defn is-location-equal?
+  [l1 l2]
+  (and (= (:x l1) (:x l2)) (= (:y l1) (:y l2))))
+
+(defn mk-soldier 
+  [name & {:keys [life location] :or {life 100 location {:x 0 :y 0}}}]
+  {:name name :location location :strenght 10 :life life})
+
+(defn is-dead?
+  [s]
+  (> 1 (:life s)))
+
+(defn- go
+  [s dir]
+  (let [d (if (contains? #{:left :right} dir) :x :y)
+        f (if (contains? #{:left :up} dir) dec inc)
+        next-pos (f (get-in s [:location d]))]
+     (if (and (> size next-pos) (not (neg? next-pos)))
+        (update-in s [:location d] f)
+        s)))
+
+(defn go-left
+  [s]
+  (go s :left))
+
+(defn go-up
+  [s]
+  (go s :up))
+
+(defn go-right
+  [s]
+  (go s :right))
+
+(defn go-down
+  [s]
+  (go s :down))
+
+(defn fight
+ [sol1 sol2]
+ (let [attack (fn [attacker attacked] 
+                  (let [punch (rand-int (:strenght @attacker))]
+                    (swap! attacked update :life - punch)))
+       s1 (atom sol1)
+       s2 (atom sol2)]
+   (do 
+    (while (every? false? (map is-dead? [@s1 @s2]))
+      (attack s1 s2)
+      (println (:name @s1) " attacked --> " (:name @s2) " life: " (:life @s2))
+      (attack s2 s1))
+      (println (:name @s2) " attacked --> " (:name @s1) " life: " (:life @s1))
+    (if (is-dead? @s1)
+      {:win @s2 :loose @s1}
+      {:win @s1 :loose @s2}))))
+
+(defn game 
+  [s1-name s2-name]
+  (let [s1 (atom (mk-soldier s1-name :location {:x (dec size) :y 0}))
+        s2 (atom (mk-soldier s2-name :location {:x 0 :y (dec size)}))
+        random-move (fn [s] 
+                        (let [m (rand-int 4)]
+                          (case m
+                            0 (go-up s)
+                            1 (go-down s)
+                            2 (go-left s)
+                            3 (go-right s))))]
+        (do
+          (while (not (is-location-equal? (:location @s1) (:location @s2)))
+             (swap! s1 random-move)
+             (println s1-name " moved --> " @s1)
+             (swap! s2 random-move)
+             (println s2-name " moved --> " @s2))
+          (fight @s1 @s2))))
--- test/soldier/core_test.clj
+++ test/soldier/core_test.clj
@@ -2,6 +2,43 @@
   (:require [clojure.test :refer :all]
             [soldier.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn mk-sut
+  [& {:keys [name life location] :or {name "rayen" life 100 location {:x 0 :y 0}}}]
+  (mk-soldier name :life life :location location))
+
+(deftest testing-soldier-functions []
+  (testing "testing go left"
+     (let [sut (mk-sut :location {:x 1 :y 1})
+           sut (go-left sut)]
+       (is (= {:x 0 :y 1} (:location sut)))
+       (is (= {:x 0 :y 1} (:location (go-left sut))))))
+   (testing "when go right"
+     (let [sut (mk-sut :location {:x 1 :y 1})
+           sut (go-right sut)]
+       (is (= {:x 2 :y 1} (:location sut)))
+       (is (= {:x 2 :y 1} (:location (go-right sut))))))
+   (testing "testing go up"
+     (let [sut (mk-sut :location {:x 1 :y 1})
+           sut (go-up sut)]
+       (is (= {:x 1 :y 0} (:location sut)))
+       (is (= {:x 1 :y 0} (:location (go-up sut))))))
+   (testing "when go down"
+     (let [sut (mk-sut :location {:x 1 :y 1})
+           sut (go-down sut)]
+       (is (= {:x 1 :y 2} (:location sut)))
+       (is (= {:x 1 :y 2} (:location (go-down sut))))))
+  (testing "when invoke is-dead? and life is lower than 1 should return true other wise should return false"
+     (is (false? (is-dead? (mk-sut))))
+     (is (true? (is-dead? (mk-sut :life 0)))))
+  (testing "fight"
+    (let [s1 (mk-sut :name "winner" :life 1000)
+          s2 (mk-sut :name "looser" :life 50)
+          score (fight s1 s2)]
+     (is (= (:name (:win score) "winner")))
+     (is (= (:name (:losser score) "looser")))))
+  )
+
+(deftest run-game []
+  (testing "run game"
+    (is (not (nil? (game "s1" "s2"))))))
+
